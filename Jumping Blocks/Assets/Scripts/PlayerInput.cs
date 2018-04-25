875ee2e8d52c25dcf8551ac55ef00eaa2c293458
Haseeb Akbar <haseeb2000@hotmail.com>
2018-04-25 17:27:47 +0100

    Added 2D physics to the player.

---
+++ Jumping Blocks/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour {
+
+	public float thrust;
+	public Rigidbody2D rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+		
+	void FixedUpdate()
+	{
+		if(Input.GetKeyDown("space")){
+		rb.AddForce(transform.up * thrust);
+		}
+	}
+	
+}
