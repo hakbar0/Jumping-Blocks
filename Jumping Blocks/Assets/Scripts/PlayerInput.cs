4fe1f4bbabef51720647f97cc82f0dbc7f14fcdb
Haseeb Akbar <haseeb2000@hotmail.com>
2018-04-25 17:38:28 +0100

    Player only jumps when is grounded.

--- Jumping Blocks/Assets/Scripts/PlayerInput.cs
+++ Jumping Blocks/Assets/Scripts/PlayerInput.cs
@@ -6,16 +6,22 @@ public class PlayerInput : MonoBehaviour {
 
 	public float thrust;
 	public Rigidbody2D rb;
+	private bool isGrounded = true;
 
 	void Start () {
 		rb = GetComponent<Rigidbody2D>();
 	}
-		
+//  
 	void FixedUpdate()
 	{
-		if(Input.GetKeyDown("space")){
+		if(Input.GetKeyDown("space") && isGrounded == true){
 		rb.AddForce(transform.up * thrust);
+			isGrounded = false;
 		}
+	}
+
+	void OnCollisionEnter2D(Collision2D other){
+		isGrounded = true;
 	}
 	
 }
