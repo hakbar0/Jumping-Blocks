3c9dffae1539c3cef09ee0c069ae01d116e2cf82
Haseeb Akbar <haseeb2000@hotmail.com>
2018-04-25 16:17:09 +0100

    Setup initial Scene.

---
+++ Jumping Blocks/Assets/Scripts/Intro.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Intro : MonoBehaviour {
+
+	void Start () {
+		StartCoroutine (firstScene ());
+	}
+	
+	IEnumerator firstScene(){
+		yield return new WaitForSeconds (7);
+		SceneManager.LoadScene ("1_menu");
+	}
+}
