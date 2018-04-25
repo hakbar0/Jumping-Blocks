c94e74c355f4fd977744dbd35ca51ea988087d2a
Haseeb Akbar <haseeb2000@hotmail.com>
2018-04-25 17:59:54 +0100

    Able to spawn enemy and make it move.

---
+++ Jumping Blocks/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+	public Rigidbody2D rb;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rb.AddForce(transform.right * -speed);
+	}
+}
---
+++ Jumping Blocks/Assets/Scripts/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+
+	public GameObject enemy;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating("SpawnBlock", 0f, 2f);
+
+	}
+	
+	void SpawnBlock(){
+		GameObject instance = Instantiate (enemy);
+		instance.transform.position = new Vector2(0f, 0f);
+	}
+}
